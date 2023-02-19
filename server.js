165e6f21ab5e4ad1a328e62cd967366f48a62932
shandie <shancarl.belgica.3@gmail.com>
2023-02-19 10:00:37 +0800

    added node server

--- server.js
+++ server.js
@@ -4,15 +4,19 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
 const db = process.env.ATLAS_URI;
+mongoose.set('strictQuery', true);
+const dbConnect = async () => {
+  await mongoose
+    .connect(db)
+    .then(() => {
+      console.log('Connected to MDB_ATLAS');
+    })
+    .catch((err) => {
+      console.log('Error connecting to MongoDB', err);
+    });
+};
+dbConnect();
+
 app.listen(PORT, () => {
   console.log(`Server up and running on port ${PORT}`);
 });
-mongoose.set('strictQuery', true);
-mongoose
-  .connect(db)
-  .then(() => {
-    console.log('Connected to MDB_ATLAS');
-  })
-  .catch((err) => {
-    console.log('Error connecting to MongoDB', err);
-  });
