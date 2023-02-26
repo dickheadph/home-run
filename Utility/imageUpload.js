00136d53fe2089a3e51f31e6db84c406a7ae1690
shandie <shancarl.belgica.3@gmail.com>
2023-02-27 07:28:47 +0800

    added trycatch in image upload

--- Utility/imageUpload.js
+++ Utility/imageUpload.js
@@ -2,24 +2,29 @@ const cloudinary = require('cloudinary').v2;
 
 const imageUpload = async (req, folder) => {
   let imageData;
-  console.log(req.file);
-  if (req.file) {
-    cloudinary.config({
-      cloud_name: process.env.CLOUD_NAME,
-      api_key: process.env.API_KEY,
-      api_secret: process.env.API_SECRET,
-      secure: true,
-    });
+  // console.log(req.file);
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+    secure: true,
+  });
 
-    imageData = await cloudinary.uploader.upload(
-      `${process.env.DIRECTORY_PATH}/${req.file.filename}.jpg`,
-      {
-        folder: folder,
-        resource_type: 'image',
-      }
-    );
+  try {
+    if (req.file) {
+      imageData = await cloudinary.uploader.upload(
+        `${process.env.DIRECTORY_PATH}/${req.file.filename}.jpg`,
+        {
+          folder: folder,
+          resource_type: 'image',
+        }
+      );
+    }
+    return imageData.secure_url;
+  } catch (error) {
+    console.log('Failed to upload image.', error);
+    return null;
   }
-  return imageData.secure_url;
 };
 
 module.exports = imageUpload;
