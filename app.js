98a027c3ea3cec73bb05647a6ea9b5b14cc8e442
shandie <shancarl.belgica.3@gmail.com>
2023-02-26 20:01:29 +0800

    added cors

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const GlobalErrHandler = require('./Middlewares/GlobalErrHandler');
 const AppErr = require('./Middlewares/AppError');
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'https://homerun-eight.vercel.app/'],
+  })
+);
 const projectRoute = require('./Routes/projectRoute');
 const usersRoute = require('./Routes/userRoute');
 
